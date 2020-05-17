42222d99cab92081c0b6ababab049685cdaa9cfb
Ryan C. Durham <ryan@stagerightlabs.com>
2020-05-17 14:31:29 -0700

    Port to rust

--- readme.md
+++ readme.md
@@ -1,17 +1,22 @@
 # CSV to Map
 
-A CLI tool to convert a csv file to a list of elixir maps.  I found myself creating some very large maps in elixir using data from CSV files, so I wrote this script to do the conversion for me. The goal was to be able to paste the output directly into a `seeder.exs` file.
+A CLI tool to convert a csv file to a list of elixir maps.  I found myself creating some very large static maps in elixir using data from CSV files, so I wrote this script to do the conversion for me. The goal was to be able to paste the output directly into a `seeder.exs` file.
 
 ## Setup
 
 ```
 git clone git@github.com:stagerightlabs/csv_to_elixir_map.git
 cd csv_to_elixir_map
-composer install
+cargo build
 ```
 
 ## Usage
 
 ```
-php convert.php path/to/file.csv output_filename.txt
+csvmap path/to/file.csv output_file.txt
 ```
+
+## Inspiration
+
+- https://docs.rs/csv/1.1.3/csv/tutorial/index.html
+- https://raw.githubusercontent.com/BurntSushi/rust-csv/master/examples/data/uspop.csv
---
+++ src/main.rs
@@ -0,0 +1,125 @@
+extern crate rand;
+
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::collections::HashMap;
+use std::fs::File;
+use std::process;
+use std::io::prelude::*;
+use rand::Rng;
+use rand::distributions::Alphanumeric;
+
+type Record = HashMap<String, String>;
+
+// This method represents the bulk of our program. It will
+// return an error to main if something goes wrong.
+fn run() -> Result<(), Box<dyn Error>> {
+
+    // Prepare a string to serve as an output buffer of sorts
+    let mut output : String = "[\n".to_string();
+
+    // Read the CSV file
+    let file_path = get_first_arg()?;
+    let mut rdr = csv::Reader::from_path(file_path)?;
+
+    // Determine our output path
+    let output_path = get_second_arg();
+    let mut output_file = match File::create(&output_path) {
+        Err(why) => panic!("couldn't create {}: {}", output_path, why),
+        Ok(output_file) => output_file,
+    };
+
+    // Loop over each record and add it to our output file
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        let mut row : String = "%{".to_string();
+
+        // Loop over each key value pair in the record to assemble an elixir map
+        for (k, v) in &record {
+            row.push_str(k);
+            row.push_str(": ");
+            row.push_str(maybe_encapsulate(v.to_string()).as_str());
+            row.push_str(", ");
+        }
+
+        // remove the trailing comma from our row
+        row.pop();
+        row.pop();
+
+        // Close the map with a bracket and comma
+        row = row + "},";
+
+        // Add our row to the output string
+        output.push_str("  ");
+        output.push_str(row.as_str());
+        output.push_str("\n");
+    }
+
+    // remove the trailing comma from our set of rows
+    output.pop();
+    output.pop();
+
+    // Append our closing list bracket
+    output.push_str("\n]");
+
+    // Attempt to write the output content to the output file
+    match output_file.write_all(output.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", output_path, why),
+        Ok(_) => println!("successfully wrote to {}", output_path),
+    }
+
+    Ok(())
+}
+
+// Returns the first positional argument sent to this process
+// If there are none, it returns an error
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("Expected 1 argument but got none")),
+        Some(file_path) => Ok(file_path)
+    }
+}
+
+// Determine whether the users has provided a name for the
+// output file. If not we will generate a random one.
+fn get_second_arg() -> String {
+    match env::args_os().nth(2) {
+        None => random_ascii_string(10).to_string() + &".txt".to_string(),
+        Some(file_path) => file_path.into_string().unwrap()
+    }
+}
+
+// Generate a random ascii string
+// https://stackoverflow.com/a/54277357
+fn random_ascii_string(len: usize) -> String {
+    let s: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .collect();
+    return s;
+}
+
+// If a value can be parsed as a floating point number we can
+// assume that doesn't need to be encapsulated in the output.
+// Otherwise we will surround it with quotes.
+fn maybe_encapsulate(value: String) -> String {
+    match value.parse::<f64>() {
+        Ok(_) => value,
+        Err(_) => {
+            let mut ret = String::from("\"");
+
+            ret.push_str(value.as_str());
+            ret.push_str("\"");
+
+            return ret;
+        },
+    }
+}
+
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
